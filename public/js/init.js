ac0e940e644f3b4e9a1e837b55405706396f14a0
sdpadrez153 <dmata62087@gmail.com>
2019-03-11 22:09:05 -0700

    changes

---
+++ public/js/init.js
@@ -0,0 +1,11 @@
+(function($) {
+  $(function() {
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".sidenav");
+  // var instances = M.Sidenav.init(elems, options);
+});
